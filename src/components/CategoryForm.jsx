b79c1983ecef8e231913401ec08ace87a065d962
philipagak <philipagak@gmail.com>
2026-09-17 08:23:17 -0400

    update project

--- src/components/CategoryForm.jsx
+++ src/components/CategoryForm.jsx
@@ -13,29 +13,62 @@ import {
   FormLabel,
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { toast } from 'react-hot-toast';
+import { db } from '../firebase';
+import { addDoc, collection } from "firebase/firestore";
 
 function CategoryForm() {
     const {isOpen, onOpen, onClose} = useDisclosure()
     const [isLoading,setIsLoading ] = useState(false);
     const[name , setName] = useState("")
+    const [error, setError] = useState(""); 
+
+    const handleCreateProduct = async (e) => {
+    e.preventDefault();
+    try {
+      setIsLoading(true);
+      setError("");
+
+      const collectionRef = collection(db, "categories");
+
+      const newCategory = {
+        name,
+        createdOn: Date.now(),
+        updatedOn: Date.now()
+      }
+
+      await addDoc(collectionRef, newProduct)
+
+      toast.success("product created successfully")
+      onClose()
+      setProduct({name:"", category:"", stock:""})
+    } catch (err) {
+      console.error(err);
+      setError("Failed to create product");
+
+      toast.error("Failed to create product");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
       <>
-      <Button
-       onClick={onOpen}>Open Modal</Button>
+      <Button m={"10px"}
+       onClick={onOpen}>Create Category</Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Create Category</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <form id="create-product" onSubmit={()=>{}}>
+            <form id="create-category" onSubmit={()=>{}}>
               <FormControl 
               
               >
                 <FormLabel>Name</FormLabel>
                 <Input type="text"
-                placeholder="e.g.laptop"
+                placeholder="e.g. electronics"
                 value={name}
               onChange={(e) => setName(e.target.value) }
                  />
@@ -45,12 +78,12 @@ function CategoryForm() {
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button variant={"ghost"} mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button type="submit" form="create-product" colorScheme={"blue"}>
+            <Button type="submit" form="create-category" colorScheme={"blue"}>
             
-           {isLoading ? " Creating product..." : " Create product"}
+           {isLoading ? " Creating category..." : " Create category"}
           </Button>
           </ModalFooter>
         </ModalContent>
